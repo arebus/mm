b4e88ca180ea6ba17ff0e829654f570a1f19065b
Aleksandr Rebenok <aleksandrr@techfinancials.com>
2017-01-13 17:37:30 +0200

    initital

--- config/config.go
+++ config/config.go
@@ -8,6 +8,7 @@ import (
 
 type DBConnectioConfig struct {
 	Name string `json:"name"`
+	Host string `json:"host"`
 	Port int `json:"port"`
 	User string `json:"user"`
 	Password string `json:"password"`
--- mm.go
+++ mm.go
@@ -2,6 +2,9 @@ package main
 
 import "github.com/arebus/mm/config"
 import "fmt"
+import "log"
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
 
 func main() {
 	cnf := config.LoadConfig("config.json")
@@ -9,4 +12,11 @@ func main() {
 	fmt.Println(dbConfig.Name)
 	fmt.Println(dbConfig.User)
 	fmt.Println(dbConfig.Port)
+	
+	db, err := sql.Open("mysql", dbConfig.User+":"dbConfig.Password+"@"+dbConfig.Host+"/")
+	if err != nil {
+		log.Fatal("DB connection error:", err)
+	}
+
+	defer db.Close()
 }
